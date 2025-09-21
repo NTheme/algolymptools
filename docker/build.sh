7cd9e9eb828e3c5cf88b53acef4453dbee6c8b3e
NTheme <nikitin.artem.a@gmail.com>
2025-09-21 16:25:02 +0300

    Universal commits presents...

--- docker/build.sh
+++ docker/build.sh
@@ -13,35 +13,3 @@ rm -rf .git
 
 go build -C /app/polygon2ejudge -o /usr/bin/p2e .
 make -C /app/algolymp BIN_DIR=/usr/bin/algolymp
-
-mkdir -p ~/.config/polygon2ejudge
-mkdir -p ~/.config/algolymp
-
-cat > ~/.config/polygon2ejudge/user_config.yaml <<'YAML'
-version: user.2
-api_key: 
-api_secret: 
-polygon_login: 
-polygon_password:
-ejudge_login: 
-ejudge_password: 
-nolint_string: ""
-YAML
-cat > ~/.config/algolymp/config.json <<'JSON'
-{
-        "ejudge": {
-                "url": "http://ejudge/",
-                "login": "ejudge",
-                "password": "ejudge",
-                "judgesDir": "/home/judges"
-        },
-        "system": {
-                "editor": "vim"
-        },
-        "polygon": {
-                "url": "https://polygon.codeforces.com",
-                "apiKey": "",
-                "apiSecret": ""
-        }
-}
-JSON
